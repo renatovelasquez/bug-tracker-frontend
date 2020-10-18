b37193f5896730fbe9debe7553c0902e71f04218
Renato <velasquez.ren@gmail.com>
2020-10-18 02:46:36 -0400

    config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Login from "./Views/Login/login";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./Views/sidebar/sidebar";
 
 function App() {
   const { auth } = useSelector((state) => state);
-  return <>{!auth.LoggedIn ? <Login /> : <></>}</>;
+  return (
+    <Router>
+      {!auth.LoggedIn ? (
+        <Login />
+      ) : (
+        <>
+          <Sidebar />
+        </>
+      )}
+    </Router>
+  );
 }
 
 export default App;
